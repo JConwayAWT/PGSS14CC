e64034e74ca3ef5e6640c0d48efb189835edb7a7
Jeff Conway <jconway@arcwebtech.com>
2014-07-02 23:14:31 -0400

    Trying more rigorous PPG2 stuff

--- lib/python/hello.py
+++ lib/python/hello.py
@@ -1,18 +1,20 @@
 import psycopg2
 import os
 import urlparse
+import sys
 
 def main():
     if 'DATABASES' not in locals():
       DATABASES = {}
+      
     if 'DATABASE_URL' in os.environ:
       url = urlparse.urlparse(os.environ['DATABASE_URL'])
 
       DATABASES['default'] = DATABASES.get('default', {})
       connection = psycopg2.connect("dbname='"+url.path[1:]+"' user='"+url.username+"' host='"+url.hostname+"' password='"+url.password+"'")
-
-    import sys
-    print sys.version
+      print sys.version
+    else:
+      print "DATABASE_URL is missing"
 
 if __name__ == '__main__':
     main()
