b1eb403aa19ee69a813e18f3770c8f67e0c5032e
Jeff Conway <jconway@arcwebtech.com>
2014-07-02 23:10:08 -0400

    Trying more psycopg2 stuff

--- lib/python/hello.py
+++ lib/python/hello.py
@@ -1,6 +1,16 @@
 import psycopg2
+import os
+import urlparse
 
 def main():
+    if 'DATABASES' not in locals():
+      DATABASES = {}
+    if 'DATABASE_URL' in os.environ:
+      url = urlparse.urlparse(os.environ['DATABASE_URL'])
+
+      DATABASES['default'] = DATABASES.get('default', {})
+      connection = psycopg2.connect("dbname='"+url.path[1:]+"' user='"+url.username+"' host='"+url.hostname+"' password='"+url.password+"'")
+
     import sys
     print sys.version
 
