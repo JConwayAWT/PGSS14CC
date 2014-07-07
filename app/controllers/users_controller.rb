534e6b3fe530c6ffae48c18d6658f6152c97f5ff
Jeff Conway <jconway@arcwebtech.com>
2014-07-07 13:36:57 -0400

    Send and parse rails environment to python script

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ require 'task'
 
 class UsersController < ApplicationController
   def index
-   x = `python lib/python/hello.py`
+   x = `python lib/python/hello.py #{ENV["RAILS_ENV"]}`
    @output = x
   end
 end
