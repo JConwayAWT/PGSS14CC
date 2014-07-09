87397741cf5cacaaac37662dcd3e34369a7a0645
Jeff Conway <jconway@arcwebtech.com>
2014-07-08 21:54:02 -0400

    Better organization for database file

--- lib/python/hello.py
+++ lib/python/hello.py
@@ -1,11 +1,12 @@
-import database_connect as db_connect
+from database import database_connect as db_connect
 import sys
+import os
 
 def main():
 
     rails_environment = sys.argv[1]
     connection = db_connect.connect_to_database(rails_environment)
-    print "Current Python version is " + sys.version
+    print "Current Python version is " + os.getcwd()
 
 if __name__ == '__main__':
     main()
