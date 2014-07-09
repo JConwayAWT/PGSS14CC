6f7987859b58c787b958b3a5ef27ba359e27da9e
Jeff Conway <jconway@arcwebtech.com>
2014-07-08 20:57:48 -0400

    Updated gitignore file again; included database_connect.py file and used it within hello.py for better organization

--- lib/python/hello.py
+++ lib/python/hello.py
@@ -1,27 +1,11 @@
-import psycopg2
-import os
-import urlparse
+import database_connect as db_connect
 import sys
 
 def main():
 
     rails_environment = sys.argv[1]
-
-    if rails_environment == "production":
-      if 'DATABASES' not in locals():
-        DATABASES = {}
-
-      if 'DATABASE_URL' in os.environ:
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-
-        DATABASES['default'] = DATABASES.get('default', {})
-        connection = psycopg2.connect("dbname='"+url.path[1:]+"' user='"+url.username+"' host='"+url.hostname+"' password='"+url.password+"'")
-        print sys.version
-      else:
-        print "DATABASE_URL is missing"
-    else:
-      connection = psycopg2.connect("dbname='pgss_14_cc_dev' user='postgres' password='password'")
-      print sys.version
+    connection = db_connect.connect_to_database(rails_environment)
+    print "Current Python version is " + sys.version
 
 if __name__ == '__main__':
     main()
