86a5dbea30240ef4e56d2f95cc84d78845479ca4
Martin Schneider <mfranzs123@gmail.com>
2014-07-10 14:46:58 -0400

    Sorted into some modules

---
+++ lib/python/solvers/TravelingSalesmanSolver.py
@@ -0,0 +1,33 @@
+#-------------------------------------------------------------------------------
+# Name:        Traveling Salesman Solver
+# Purpose:     Traveling salesman base solver
+#
+# Author:      Martin Schneider
+#
+# Created:     07/09/2014
+# Copyright:   (c) Martin 2014
+# Licence:     Creative Commons (CC)
+#-------------------------------------------------------------------------------
+
+import Coordinate
+import json
+
+class TravelingSalesmanSolver:
+	cords = []
+	answer=""
+
+	def __init__(self, params):
+		data = json.loads(params)
+		for c in range(0,len(data['x'])):
+			cord = Coordinate.Coordinate(data['x'][c], data['y'][c], c)
+			self.cords.append(cord)
+
+	def solve(self):
+		return "No solution implemented!"
+
+
+
+		
+
+
+	
