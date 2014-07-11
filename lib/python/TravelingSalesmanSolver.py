c5278ffb5ef6d3573fc599f10f7d2ffb5a96f81e
Jeff Conway <jconway@arcwebtech.com>
2014-07-10 21:56:57 -0400

    Removing conflict marks

--- lib/python/TravelingSalesmanSolver.py
+++ lib/python/TravelingSalesmanSolver.py
@@ -13,35 +13,14 @@ import Coordinate
 import json
 
 class TravelingSalesmanSolver:
-<<<<<<< HEAD
-    cords = []
-    answer=""
-
-    def __init__(self, params = None):
-        if params == None:
-            return
-        data = json.loads(params)
-        for c in range(0,len(data['x'])):
-=======
 	cords = []
 	answer=""
 
 	def __init__(self, params):
 		data = json.loads(params)
 		for c in range(0,len(data['x'])):
->>>>>>> e83d1c30ccb7f76513547fed35dba64196a9554f
 			cord = Coordinate.Coordinate(data['x'][c], data['y'][c], c)
 			self.cords.append(cord)
 
 	def solve(self):
-<<<<<<< HEAD
 		return "No solution implemented!"
-
-
-
-
-
-
-=======
-		return "No solution implemented!"
->>>>>>> e83d1c30ccb7f76513547fed35dba64196a9554f
