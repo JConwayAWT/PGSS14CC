cb1ef0430d792e8b5c0144bada0fdde668afa82d
Zachary D. Pozun <ZacharyDPozun@gmail.com>
2014-07-29 13:03:36 -0400

    fixed issue in thermostat

--- lib/python/multimetallics/solvers/MDbasinhop.py
+++ lib/python/multimetallics/solvers/MDbasinhop.py
@@ -21,6 +21,7 @@ import json
 import NanoClass
 import ase
 from ase.md.nvtberendsen import NVTBerendsen
+from ase.md.verlet import VelocityVerlet
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase import units
 from ase.optimize import FIRE
@@ -35,9 +36,9 @@ class MDSolver(MetalicsSolver.MetalicFoldingSolver):
     self.reCenter()
     self.bestEnergy = self.particle.get_potential_energy()
     self.bestParticle = deepcopy(self.particle)
-    berendsen = NVTBerendsen(self.particle, 2.5 * units.fs, 3000, taut=0.5*1000*units.fs)
+    berendsen = NVTBerendsen(self.particle, 2.5 * units.fs, 1500, taut=0.5*1000*units.fs)
     dyn = FIRE(atoms=self.particle)
-    MaxwellBoltzmannDistribution(self.particle,3000)
+    MaxwellBoltzmannDistribution(self.particle,1500*units.kB)
     CALCULATIONS=100
     for i in range(CALCULATIONS):
       if i%1==0:
@@ -46,15 +47,22 @@ class MDSolver(MetalicsSolver.MetalicFoldingSolver):
         self.checkTimeout()
         self.setSolution(self.getAnswer())
       self.particle.rattle(stdev=0.1)
-      MaxwellBoltzmannDistribution(self.particle,3000)
+      MaxwellBoltzmannDistribution(self.particle,1500*units.kB)
       self.particle.get_potential_energy()
-      berendsen.run(1000)
+      berendsen.run(500)
       dyn.run()
       self.reCenter()
       testEnergy = self.particle.get_potential_energy()
       if (testEnergy < self.bestEnergy):
         self.bestEnergy = testEnergy
         self.bestParticle = deepcopy(self.particle)
+      else:
+        self.particle = NanoClass.genParticle(self.definingString, int(self.numberOfAtoms))
+        MaxwellBoltzmannDistribution(self.particle,1500*units.kB)
+        self.particle.get_potential_energy()
+        if (testEnergy < self.bestEnergy):
+          self.bestEnergy = testEnergy
+          self.bestParticle = deepcopy(self.particle)
     return self.getAnswer()
 
   def getAnswer(self):
